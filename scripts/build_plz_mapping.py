784a63a07de50e09c433b40dba6bfd2af590c017
bba161 <b.balzereit@sz-sb.de>
2026-07-07 15:41:20 +0200

    Update build_plz_mapping.py

--- scripts/build_plz_mapping.py
+++ scripts/build_plz_mapping.py
@@ -81,7 +81,7 @@ def load_saarland_plz_zauberware() -> list[dict]:
     
     # Firmen-Keywords, die wir rausfiltern wollen
     firmen_keywords = [
-        "AG", "Media", "Landesamt", "AOK", "GmbH", "KG", "Versicherung", "Agentur", "Bank", "Post",
+        "AG", "Media", "IHK", "Landesamt", "AOK", "GmbH", "KG", "Versicherung", "Agentur", "Bank", "Post",
         "Direkt", "Service", "Regio", "Deutsche", "Universität", "Klinik",
         "Rundfunk", "Lotterie", "reha", "Rentenversicherung", "HUK",
         "Innungskrankenkasse", "UKV", "Saarländischer", "Praktiker",
@@ -129,7 +129,7 @@ def load_saarland_plz_opendatasoft() -> list[dict]:
     payload = resp.json()
     
     firmen_keywords = [
-        "AG", "Media", "AOK", "Landesamt", "GmbH", "KG", "Versicherung", "Agentur", "Bank", "Post",
+        "AG", "Media", "AOK", "IHK", "Landesamt", "GmbH", "KG", "Versicherung", "Agentur", "Bank", "Post",
         "Direkt", "Service", "Regio", "Deutsche", "Universität", "Klinik",
         "Rundfunk", "Lotterie", "reha", "Rentenversicherung", "HUK",
         "Innungskrankenkasse", "UKV", "Saarländischer", "Praktiker",
